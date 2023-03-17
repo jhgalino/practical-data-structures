b0749f03a7f8c108e4ef645059b19d39c93fd220
John Henry Galino <github.stolze@simplelogin.com>
2023-03-17 12:57:47 +0800

    Optimize dedup.js

--- dedup.js
+++ dedup.js
@@ -3,21 +3,17 @@ const data = JSON.parse(fs.readFileSync("data/users.json"));
 
 function dedup(data) {
 
-    const idStore = new Set();
-    const filtered = [];
+    const filtered = new Map();
 
     // We need to use a stack for this because using splice changes the length of
     // the array. Changing the length of the array also changes the indexes of the
     // elements, thus making splice delete the wrong item. As such, we use an Array
     // as a stack.
     data.forEach(v => {
-        if (!idStore.has(v.id)) {
-            idStore.add(v.id);
-            filtered.push(v);
+        if (!filtered.has(v.id)) {
+            filtered.set(v.id, v);
         }
     });
 
-    return filtered;
+    return Array.from(filtered.values())
 }
-
-console.log(dedup(data));
