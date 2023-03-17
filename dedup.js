11edd0908655d9373567e31526b6f303ae17cf39
John Henry Galino <github.stolze@simplelogin.com>
2023-03-17 10:29:10 +0800

    Add deduplication function

---
+++ dedup.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+let data = JSON.parse(fs.readFileSync("data/users.json"));
+
+function dedup(data) {
+
+    let idStore = new Set();
+
+    console.log(data.length);
+
+    data.map((v, i) => {
+        if (!idStore.has(v.id)) {
+            idStore.add(v.id);
+        } else {
+            console.log(v.id)
+            data.splice(i, 1);
+        }
+    });
+
+    console.log(data.length);
+
+    return data;
+}
+
+dedup(data);
