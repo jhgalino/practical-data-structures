32db479c23ebc441e53ef779f4e0c588049b040f
John Henry Galino <github.stolze@simplelogin.com>
2023-03-17 11:42:11 +0800

    Change comment style

--- dedup.js
+++ dedup.js
@@ -6,10 +6,10 @@ function dedup(data) {
     const idStore = new Set();
     const filtered = new Array();
 
-    /* We need to use a stack for this because using splice changes the length of
-    the array. Changing the length of the array also changes the indexes of the
-    elements, thus making splice delete the wrong item. As such, we use an Array
-    as a stack. */
+    // We need to use a stack for this because using splice changes the length of
+    // the array. Changing the length of the array also changes the indexes of the
+    // elements, thus making splice delete the wrong item. As such, we use an Array
+    // as a stack.
     data.map((v, _) => {
         if (!idStore.has(v.id)) {
             idStore.add(v.id);
